d62e25eb809680c8cdf796e87022a9cb2281d241
PRA945 <prannunziata@quinnipiac.edu>
2015-10-01 18:09:56 +0000

    Add question create view

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ class Home(TemplateView):
     template_name = "home.html"
 
 class QuestionCreateView(CreateView):
-  model * Question
+  model = Question
   template_name = "question/question_form.html"
   fields = ['title', 'description']
   success_url = reverse_lazy('home')
